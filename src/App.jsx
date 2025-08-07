43bb0bc8a2f8cd96fb38c8cd4d55f984642612be
Heikey <joaofialho123123@gmail.com>
2025-08-07 01:06:10 -0300

    Search Bar: Added the Base of search bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,41 @@ import NewProduct from "./components/NewProduct";
 import ProductList from "./components/ProductList";
 
 function App() {
-  const [newProductModal, setNewProductModal] = useState(false);
-  const [products, setProducts] = useState(() => {
-    return JSON.parse(localStorage.getItem("products")) || [];
-  });
-  const atualizarProdutos = () => {
-    const produtosSalvos = JSON.parse(localStorage.getItem("products")) || [];
-    setProducts(produtosSalvos);
-  };
-  console.log("📦 Produtos cadastrados:", products);
+	const [newProductModal, setNewProductModal] = useState(false);
+	const [products, setProducts] = useState(() => {
+		return JSON.parse(localStorage.getItem("products")) || [];
+	});
+	const [filter, setFilter] = useState('');
+	const atualizarProdutos = () => {
+		const produtosSalvos = JSON.parse(localStorage.getItem("products")) || [];
+		setProducts(produtosSalvos);
+	};
+	console.log("📦 Produtos cadastrados:", products);
 
-  return (
-    <main className="bg-amber-50 py-7 px-8 min-h-[100vh]">
-      <h1 className="font-bold text-3xl text-center">Lanche do IF</h1>
-      <div className="flex flex-col mt-4">
-        <button
-          className="w-fit mx-auto bg-amber-900 hover:bg-amber-950 text-white p-3 rounded-xl"
-          onClick={() => setNewProductModal(true)}
-        >
-          Cadastrar Novo Produto
-        </button>
-      </div>
+	return (
+		<main className="bg-amber-50 py-7 px-8 min-h-[100vh]">
+			<h1 className="font-bold text-3xl text-center">Lanche do IF</h1>
+			<div className="flex flex-row justify-evenly mt-4">
+				<input className="border rounded-lg w-100 p-1" type="search" onChange={(e) => setFilter(e.target.value)}></input>
+				<button
+					className="w-fit  bg-amber-900 hover:bg-amber-950 text-white p-3 rounded-xl"
+					onClick={() => setNewProductModal(true)}
+				>
+					Cadastrar Novo Produto
+				</button>
+			</div>
 
-      <div className="mt-10">
-        <ProductList products={products}/>
-      </div>
-      {newProductModal && (
-        <NewProduct
-          setNewProductModal={setNewProductModal}
-          atualizarProdutos={atualizarProdutos}
-        />
-      )}
-    </main>
-  );
+			<div className="mt-10">
+				<ProductList products={products} filter={filter} />
+			</div>
+			{newProductModal && (
+				<NewProduct
+					setNewProductModal={setNewProductModal}
+					atualizarProdutos={atualizarProdutos}
+				/>
+			)}
+		</main>
+	);
 }
 
 export default App;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,18 +2,20 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 
-const ProductList = ({ products }) => {
-  return (
-    <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {products.length === 0 ? (
-        <p className="text-center text-gray-400"></p>
-      ) : (
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))
-      )}
-    </div>
-  );
+const ProductList = ({ products, filter }) => {
+	return (
+		<div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+			{products.length === 0 ? (
+				<p className="text-center text-gray-400"></p>
+			) : (
+				products
+					.filter((product) => product.name.includes(filter) || product.description.includes(filter))
+					.map((product) => (
+						<ProductCard key={product.id} product={product} />
+					))
+			)}
+		</div>
+	);
 };
 
 export default ProductList;
